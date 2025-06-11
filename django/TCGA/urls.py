643dd2b2170f1def4caf510d2c3de0ccab3b196f
Anne Haley <anne.haley@kitware.com>
2025-06-11 17:33:25 -0400

    Create basic templates

--- django/TCGA/urls.py
+++ django/TCGA/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('images/<int:image_id>', views.image_view, name='image_view'),
+    path('', views.index, name='index'),
 ]
--- django/TCGA/views.py
+++ django/TCGA/views.py
@@ -1,5 +1,21 @@
-from django.http import HttpResponse
+import json
+from django.shortcuts import render
+
+from .models import Image, Nucleus
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the TCGA index.")
+    images = Image.objects.all()
+    return render(request, 'index.html', dict(images=images))
+
+
+def image_view(request, image_id):
+    image = Image.objects.get(id=image_id)
+    nuclei = Nucleus.objects.filter(image=image)
+    return render(request, 'image_view.html', dict(
+        tile_url=image.tile_url,
+        nuclei=json.dumps([
+            dict(x=n.x, y=n.y, width=n.width, height=n.height, orientation=n.orientation)
+            for n in nuclei
+        ])
+    ))
