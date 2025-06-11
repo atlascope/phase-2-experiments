a6f6a03c8dbccbe6f6e7892eb0416427caa99879
Anne Haley <anne.haley@kitware.com>
2025-06-11 15:55:25 -0400

    Create Image and Nucleus models

--- django/TCGA/models.py
+++ django/TCGA/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    name = models.CharField(max_length=255)
+    tile_url = models.CharField(max_length=500)
+
+
+class Nucleus(models.Model):
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    x = models.FloatField()
+    y = models.FloatField()
+    width = models.FloatField()
+    height = models.FloatField()
+    orientation = models.FloatField()
+
+    class Meta:
+        verbose_name_plural = "nuclei"
